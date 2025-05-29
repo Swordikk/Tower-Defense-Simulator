30af042da3b356fcb20928f797ea77f10dae7158
Swordikk <133685730+Swordikk@users.noreply.github.com>
2025-05-29 19:38:50 +0400

    Update TDS.lua

--- TDS.lua
+++ TDS.lua
@@ -176,7 +176,7 @@ Toggle:OnChanged(function(Value)
     while _G.AutoFarm do wait(0.5)
 		Dropdown:OnChanged(function(Value)
 			if Value == "Crossroads(Crook Boss, Trapper)" then
-				while #Towers:GetChildren() ~= 1 and _G.AutoFarm do wait(0.5)
+				while #Towers:GetChildren() ~= 1 and _G.AutoFarm do wait(2)
 					placeTower("Crook Boss", Vector3.new(11.4159966, 2.34999824, 10.5649462, 0, 0, 0, 0, 1, 0, 0, 0, 0))
 				end
 				while Towers:GetChildren()[1].Upgrades["2"].EarPiece.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
@@ -230,146 +230,62 @@ Toggle:OnChanged(function(Value)
 				while #Towers:GetChildren() ~= 0 and _G.AutoFarm do
 					wait(1)
 				end
-				wait(5)
 			elseif Value == "Pulluted Wasteland 2(Brawler, Trapper)" then
-				while #Towers:GetChildren() ~= 1 and _G.AutoFarm do wait(0.5)
+				while #Towers:GetChildren() == 0 and _G.AutoFarm do wait(2)
 					placeTower("Brawler", Vector3.new(1.12966609, 2.35000801, -7.74175596, 0.331033051, 0, 0.943619132, 0, 1, 0, -0.943619251, 0, 0.331033021))
 				end
-				while #Towers:GetChildren() ~= 2 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Brawler", Vector3.new(-1.06129456, 2.35000801, -7.76885509, -0.237911835, 0, 0.971286774, 0, 1, 0, -0.971286774, 0, -0.237911835))
-				end
-				while #Towers:GetChildren() ~= 3 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Brawler", Vector3.new(-0.81419903, 2.35000801, -9.76670456, -0.453678876, 0, 0.891165197, 0, 1, 0, -0.891165316, 0, -0.453678817))
-				end
-				while #Towers:GetChildren() ~= 4 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Brawler", Vector3.new(1.23481107, 2.35000801, -9.78998184, -0.148538768, 0, 0.988906622, 0, 1.00000012, 0, -0.988906622, 0, -0.148538768))
-				end
-				while #Towers:GetChildren() ~= 5 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Brawler", Vector3.new(3.26371861, 2.35000801, -7.73256111, -0.654829919, 0, 0.755776286, 0, 1, 0, -0.755776405, 0, -0.65482986))
-				end
-				while #Towers:GetChildren() ~= 6 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Brawler", Vector3.new(3.27758455, 2.35000801, -9.73951721, -0.890398204, 0, 0.455182493, 0, 1, 0, -0.455182493, 0, -0.890398204))
-				end
-				while #Towers:GetChildren() ~= 7 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-15.3021049, 2.35000801, -2.64966488, 0.769444406, 0, -0.638713777, -0, 1, -0, 0.638713777, 0, 0.769444406))
-				end
-				while #Towers:GetChildren() ~= 8 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-18.3173981, 2.35000801, -2.58554173, 0.388187438, 0, 0.921580434, -0, 1, -0, -0.921580434, 0, 0.388187438))
-				end
-				while #Towers:GetChildren() ~= 9 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-16.7420273, 2.4000001, -0.00460910797, 0.805656791, 0, 0.59238261, -0, 1, -0, -0.59238261, 0, 0.805656791))
-				end
-				while #Towers:GetChildren() ~= 10 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-21.3622169, 2.35000801, -2.67031097, -0.861847937, 0, 0.507166803, 0, 1, -0, -0.507166803, 0, -0.861847937))
-				end
-				while #Towers:GetChildren() ~= 11 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-19.9067879, 2.4000001, -0.0217428207, -0.426200151, 0, 0.904628873, 0, 1.00000012, -0, -0.904628992, 0, -0.426200092))
-				end
-				while #Towers:GetChildren() ~= 12 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					placeTower("Trapper", Vector3.new(-22.3155594, 2.35000801, -7.70352173, 1, 0, 0, 0, 1, 0, 0, 0, 1))
-				end
-				while Towers:GetChildren()[7].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[7])
-				end
-				while Towers:GetChildren()[8].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[8])
-				end
-				while Towers:GetChildren()[9].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[9])
-				end
-				while Towers:GetChildren()[10].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[10])
-				end
-				while Towers:GetChildren()[11].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[11])
-				end
-				while Towers:GetChildren()[12].Upgrades["1"].goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[12])
-				end
-				while Towers:GetChildren()[7].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[7])
-				end
+				whilePlaceTower("Brawler", Vector3.new(-1.06129456, 2.35000801, -7.76885509, -0.237911835, 0, 0.971286774, 0, 1, 0, -0.971286774, 0, -0.237911835), 2)
+				whilePlaceTower("Brawler", Vector3.new(-0.81419903, 2.35000801, -9.76670456, -0.453678876, 0, 0.891165197, 0, 1, 0, -0.891165316, 0, -0.453678817), 3)
+				whilePlaceTower("Brawler", Vector3.new(1.23481107, 2.35000801, -9.78998184, -0.148538768, 0, 0.988906622, 0, 1.00000012, 0, -0.988906622, 0, -0.148538768), 4)
+				whilePlaceTower("Brawler", Vector3.new(3.26371861, 2.35000801, -7.73256111, -0.654829919, 0, 0.755776286, 0, 1, 0, -0.755776405, 0, -0.65482986), 5)
+				whilePlaceTower("Brawler", Vector3.new(3.27758455, 2.35000801, -9.73951721, -0.890398204, 0, 0.455182493, 0, 1, 0, -0.455182493, 0, -0.890398204), 6)
+				whilePlaceTower("Trapper", Vector3.new(-15.3021049, 2.35000801, -2.64966488, 0.769444406, 0, -0.638713777, -0, 1, -0, 0.638713777, 0, 0.769444406), 7)
+				whilePlaceTower("Trapper", Vector3.new(-18.3173981, 2.35000801, -2.58554173, 0.388187438, 0, 0.921580434, -0, 1, -0, -0.921580434, 0, 0.388187438), 8)
+				whilePlaceTower("Trapper", Vector3.new(-16.7420273, 2.4000001, -0.00460910797, 0.805656791, 0, 0.59238261, -0, 1, -0, -0.59238261, 0, 0.805656791), 9)
+				whilePlaceTower("Trapper", Vector3.new(-21.3622169, 2.35000801, -2.67031097, -0.861847937, 0, 0.507166803, 0, 1, -0, -0.507166803, 0, -0.861847937), 10)
+				whilePlaceTower("Trapper", Vector3.new(-19.9067879, 2.4000001, -0.0217428207, -0.426200151, 0, 0.904628873, 0, 1.00000012, -0, -0.904628992, 0, -0.426200092), 11)
+				whilePlaceTower("Trapper", Vector3.new(-22.3155594, 2.35000801, -7.70352173, 1, 0, 0, 0, 1, 0, 0, 0, 1), 12)
+				whileUpgradeTower(Towers:GetChildren()[7], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[8], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[9], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[10], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[11], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[12], "1", "goggles")
+				whileUpgradeTower(Towers:GetChildren()[7], "2", "Torso1")
 				Landmine(7)
-				while Towers:GetChildren()[8].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[8])
-				end
+				whileUpgradeTower(Towers:GetChildren()[8], "2", "Torso1")
 				Landmine(8)
-				while Towers:GetChildren()[9].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[9])
-				end
+				whileUpgradeTower(Towers:GetChildren()[9], "2", "Torso1")
 				Landmine(9)
-				while Towers:GetChildren()[10].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[10])
-				end
+				whileUpgradeTower(Towers:GetChildren()[10], "2", "Torso1")
 				Landmine(10)
-				while Towers:GetChildren()[11].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[11])
-				end
+				whileUpgradeTower(Towers:GetChildren()[11], "2", "Torso1")
 				Landmine(11)
-				while Towers:GetChildren()[12].Upgrades["2"].Torso1.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[12])
-				end
+				whileUpgradeTower(Towers:GetChildren()[12], "2", "Torso1")
 				Landmine(12)
-				while Towers:GetChildren()[1].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[1])
-				end
-				while Towers:GetChildren()[2].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[2])
-				end
-				while Towers:GetChildren()[3].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[3])
-				end
-				while Towers:GetChildren()[4].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[4])
-				end
-				while Towers:GetChildren()[5].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[5])
-				end
-				while Towers:GetChildren()[6].Upgrades["1"].Goggles.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[6])
-				end
-				while Towers:GetChildren()[1].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[1])
-				end
-				while Towers:GetChildren()[2].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[2])
-				end
-				while Towers:GetChildren()[3].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[3])
-				end
-				while Towers:GetChildren()[4].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[4])
-				end
-				while Towers:GetChildren()[5].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[5])
-				end
-				while Towers:GetChildren()[6].Upgrades["2"].R6_Torso.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[6])
-				end
-				while Towers:GetChildren()[1].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[1])
-				end
-				while Towers:GetChildren()[2].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[2])
-				end
-				while Towers:GetChildren()[3].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[3])
-				end
-				while Towers:GetChildren()[4].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[4])
-				end
-				while Towers:GetChildren()[5].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[5])
-				end
-				while Towers:GetChildren()[6].Upgrades["3"].Plane.Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-					upgradeTower(Towers:GetChildren()[6])
-				end
+				whileUpgradeTower(Towers:GetChildren()[1], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[2], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[3], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[4], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[5], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[6], "1", "Goggles")
+				whileUpgradeTower(Towers:GetChildren()[1], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[2], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[3], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[4], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[5], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[6], "2", "R6_Torso")
+				whileUpgradeTower(Towers:GetChildren()[1], "3", "Plane")
+				whileUpgradeTower(Towers:GetChildren()[2], "3", "Plane")
+				whileUpgradeTower(Towers:GetChildren()[3], "3", "Plane")
+				whileUpgradeTower(Towers:GetChildren()[4], "3", "Plane")
+				whileUpgradeTower(Towers:GetChildren()[5], "3", "Plane")
+				whileUpgradeTower(Towers:GetChildren()[6], "3", "Plane")
 				while #Towers:GetChildren() ~= 0 and _G.AutoFarm do
 					wait(1)
 				end
-				wait(5)
 			elseif Value == "U_Turn(Shotgunner)" then
-				while #Towers:GetChildren() == 0 and _G.AutoFarm do wait(0.5)
+				while #Towers:GetChildren() == 0 and _G.AutoFarm do wait(2)
 					placeTower("Shotgunner", Vector3.new(15.6670189, 2.35, 14.7578468, 0.42744875, 0, -0.904039621, -0, 1, -0, 0.904039621, 0, 0.42744875))
 				end
 				whileUpgradeTower(Towers:GetChildren()[1], "1", "Glove")
@@ -416,20 +332,8 @@ Toggle:OnChanged(function(Value)
 				while Towers:GetChildren()[5].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
 				    upgradeTower(Towers:GetChildren()[5])
 				end
-				while Towers:GetChildren()[6].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-				    upgradeTower(Towers:GetChildren()[6])
-				end
-				while Towers:GetChildren()[7].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-				    upgradeTower(Towers:GetChildren()[7])
-				end
-				while Towers:GetChildren()[8].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-				    upgradeTower(Towers:GetChildren()[8])
-				end
-				while Towers:GetChildren()[9].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-				    upgradeTower(Towers:GetChildren()[9])
-				end
-				while Towers:GetChildren()[10].Upgrades["3"].Vest:GetChildren()[1].Transparency == 1 and #Towers:GetChildren() ~= 0 and _G.AutoFarm do wait(0.5)
-				    upgradeTower(Towers:GetChildren()[10])
+				while #Towers:GetChildren() ~= 0 and _G.AutoFarm do
+					wait(1)
 				end
 			end
 		end)
